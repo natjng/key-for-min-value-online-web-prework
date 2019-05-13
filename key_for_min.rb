d8c73ded61a7ac2fb61c4d8614663cdb255a8640
Natalie J <natsetc@gmail.com>
2019-05-13 08:19:22 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -11,8 +11,8 @@ def key_for_min_value(name_hash)
   while i < arr_v.length
       if arr_v[i] > arr_v[i+1]
         arr_v[i], arr_v[i+1] = arr_v[i+1], arr_v[i]
-      i +=1
       end
+      i +=1
   end
   value = arr_v[0]
   name_hash.collect do
