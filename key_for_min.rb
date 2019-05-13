c9e5ded3dc823504c3da651546443f12826af313
Natalie J <natsetc@gmail.com>
2019-05-13 08:21:15 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -11,8 +11,6 @@ def key_for_min_value(name_hash)
   while i < arr_v.length
       if arr_v[i] > arr_v[i+1]
         arr_v[i], arr_v[i+1] = arr_v[i+1], arr_v[i]
-      else
-        nil
       end
       i +=1
   end
