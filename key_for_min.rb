8b74125a66459254ad81ecee3406b205acceccf2
Natalie J <natsetc@gmail.com>
2019-05-13 08:11:45 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,21 +5,20 @@ def key_for_min_value(name_hash)
   if name_hash.length == 0
     return nil
   end
-  array_of_v = []
-  array_of_v = name_hash.collect do
+  arr_v = []
+  arr_v = name_hash.collect do
     |k,v|
     v
   end
   i = 0
-  sorted_array_of_v = []
-  while i < array_of_v.length
+  while i < arr_v.length
     array_of_v.collect do |i|
       temp_array = array_of_v.dup
       temp_array[i], temp_array[i+1] = temp_array[i+1], temp_array[i]
       sorted_array_of_v << temp_array
       #sorted_array_of_v
-      if array_of_v[i] > array_of_v[i+1}
-        array_of_v_v[i], arr_v[i+1] = arr_v[i+1], arr_v[i]
+      if arr_v[i] > arr_v[i+1}
+        arr_v[i], arr_v[i+1] = arr_v[i+1], arr_v[i]
       i +=1
       end
     end
